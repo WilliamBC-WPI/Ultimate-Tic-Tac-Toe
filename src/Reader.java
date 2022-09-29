9781de860a3b1ab36627c1e7f78436fbdbe68d1d
Emmanuel Ola <54866720+eoola@users.noreply.github.com>
2022-09-29 19:42:24 -0400

    Add parser to Reader

--- src/Reader.java
+++ src/Reader.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Reader {
@@ -11,9 +13,53 @@ public class Reader {
 //    String initialMovesFile = "first_four_moves";
 //    String filename = "move_file";
 
-    public Reader() {
-
+    public static ArrayList<String> readFile(String pathname){
+        File firstFourMovesFile = new File(pathname);
+        ArrayList<String> output = new ArrayList<>();
+        if (firstFourMovesFile.exists()) {
+            try {
+                Scanner fileReader = new Scanner(firstFourMovesFile);
+                while (fileReader.hasNextLine()) {
+                    String nextLine = fileReader.nextLine();
+                    output.add(nextLine);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("File not found");
+            }
+        } else {
+            System.out.println("That don't exist my guy!");
+        }
+        return output;
     }
+
+    public static String getPlayerName(int playerPosition) {
+        playerPosition = playerPosition % 2;
+        return Reader.readFile(Main.REFEREEPATH).get(playerPosition).split(" ")[0];
+    }
+
+    public static String getPlayerNameFromMoveString(String moveString) {
+        return moveString.split(" ")[0];
+    }
+
+    /**
+     * @return An integer array where the first element is the player number, the second element is
+     * the local board where the move should be played, and the last element is the position on
+     * that board that we're playing on
+     */
+    public static int[] parseMove(String move) {
+        int localBoardPosition = Integer.parseInt(move.split(" ")[1]);
+        int positionToPlay = Integer.parseInt(move.split(" ")[2]);
+
+        if (Main.game.player1.playerName.equals(getPlayerNameFromMoveString(move))) {
+            int[] output = {1, localBoardPosition, positionToPlay};
+            return output;
+        } else {
+            int[] output = {-1, localBoardPosition, positionToPlay};
+            return output;
+        }
+    }
+
 
 
     public void readFirstMoves(Board gb) throws FileNotFoundException {
